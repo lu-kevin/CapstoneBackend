48299bddca6f25d6c082333ac88f88dd2f273aec
Kevin Lu <35882128+lu-kevin@users.noreply.github.com>
2023-03-28 19:37:00 -0400

    added socketIO server

--- app.js
+++ app.js
@@ -47,3 +47,49 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//
+
+//const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4000;
+const index = require("./routes/index");
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+
+  socket.on("measurement", (arg) => {
+   console.log("Measurement: ")
+   console.log(arg);
+   clearInterval(interval);
+ });
+
+});
+
+const getApiAndEmit = socket => {
+  //const response = new Date();
+  const response = "Received event!";
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
